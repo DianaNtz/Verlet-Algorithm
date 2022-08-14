147493292253e37c5ab2c6dfd2f4d55afdfe6dd7
Diana Nitzschke <diana.nitzschke@stud.uni-hannover.de>
2022-08-14 17:34:43 +0200

    created gif animations for equal and unequal masses

--- Kepler.py
+++ Kepler.py
@@ -4,12 +4,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
+import os
+import imageio
+filenames = []
 #some initial values
 t0=0
-tfinal=95#equal masses
-#tfinal=115#unequal masses
+#tfinal=95#equal masses
+tfinal=115#unequal masses
 dt=0.005
-m1=2*10**(26)#10**(25)
+m1=10**(25)#2*10**(26)
 m2=2*10**(26)
 G=6.67259*10**-11
 steps=int((tfinal-t0)/dt)
@@ -79,22 +82,32 @@ for i in range(0,steps+1):
     vy2n=vy2n+dt*0.5*(fy2(x1[i],y1[i],z1[i],x2[i],y2[i],z2[i])+fy2(x1n,y1n,z1n,x2n,y2n,z2n))
     vz2n=vz2n+dt*0.5*(fz2(x1[i],y1[i],z1[i],x2[i],y2[i],z2[i])+fz2(x1n,y1n,z1n,x2n,y2n,z2n))          
     t[i]=tn
-    tn=tn+dt   
-fig = plt.figure()
-ax = p3.Axes3D(fig)     
-ax.plot(x2*10**(-6), y2*10**(-6),z2*10**(-6),color='skyblue',linewidth=2)
-ax.plot(x1*10**(-6), y1*10**(-6),z1*10**(-6),color='deeppink',linestyle='-.',linewidth=2)
-ax.set_zlim(-0.5,0.5)
-ax.set_ylim(-0.75,0.75)
-ax.set_xlim(0,3.0)
-ax.set_xlabel("x in $10^3$ km",fontsize= 13,labelpad=7)
-ax.set_ylabel("y in $10^3$ km",fontsize= 13,labelpad=7)
-ax.set_zlabel("z in $10^3$ km",fontsize= 13,labelpad=7)
-ax.zaxis.set_tick_params(labelsize=13)
-ax.yaxis.set_tick_params(labelsize=13)
-ax.xaxis.set_tick_params(labelsize=13)
-ax.set_xticks([0,1,2,3])
-ax.set_yticks([-0.75,0,0.75])
-ax.set_zticks([-0.5,0,0.5])
-ax.view_init(10, 230) 
-plt.show()       
+    tn=tn+dt
+    if(i%200==0):
+       fig = plt.figure()
+       ax = p3.Axes3D(fig)     
+       ax.plot(x2[:i]*10**(-6), y2[:i]*10**(-6),z2[:i]*10**(-6),color='skyblue',linewidth=2)
+       ax.plot(x1[:i]*10**(-6), y1[:i]*10**(-6),z1[:i]*10**(-6),color='deeppink',linestyle='-.',linewidth=2)
+       ax.set_zlim(-0.5,0.5)
+       ax.set_ylim(-0.75,0.75)
+       ax.set_xlim(0,3.0)
+       ax.set_xlabel("x in $10^3$ km",fontsize= 13,labelpad=7)
+       ax.set_ylabel("y in $10^3$ km",fontsize= 13,labelpad=7)
+       ax.set_zlabel("z in $10^3$ km",fontsize= 13,labelpad=7)
+       ax.zaxis.set_tick_params(labelsize=13)
+       ax.yaxis.set_tick_params(labelsize=13)
+       ax.xaxis.set_tick_params(labelsize=13)
+       ax.set_xticks([0,1,2,3])
+       ax.set_yticks([-0.75,0,0.75])
+       ax.set_zticks([-0.5,0,0.5])
+       ax.view_init(10, 230) 
+       filename ='bla{0:.0f}.png'.format(i/200)
+       filenames.append(filename)    
+       plt.savefig(filename,dpi=150)
+       plt.close()       
+with imageio.get_writer('kepleruneuqalmasses.gif', mode='I') as writer:
+    for filename in filenames:
+        image = imageio.imread(filename)
+        writer.append_data(image)       
+for filename in set(filenames):
+    os.remove(filename)     
